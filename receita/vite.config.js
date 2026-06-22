9eb7f6e038c647c9ba1c915695ba851d47b91011
Holy Sun <diego.vieirad@gmail.com>
2026-06-22 19:22:53 -0300

    fix: resolve merge conflicts

--- receita/vite.config.js
+++ receita/vite.config.js
@@ -5,10 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-<<<<<<< HEAD
   build: {
     sourcemap: false,
   },
-=======
->>>>>>> 3d97896c1f3efb57f08a37350a6b04dc3f2de73a
 });
