a591118145eec35073e1a60bb800deac7b48967b
Holy Sun <diego.vieirad@gmail.com>
2026-07-05 21:23:51 -0300

    fix: js data

--- restaurante/casadavo/src/data/cardapioData.js
+++ restaurante/casadavo/src/data/cardapioData.js
@@ -53,7 +53,7 @@ export const cardapioDiario = [
     id: 104,
     categoria: "Bebidas",
     nome: "CocaCola",
-    descricao: "Suco 100% natural, sem adição de açúcar ou conservantes.",
+    descricao: "Refrigerante Coca-Cola lata.",
     preco: 5.5,
     img: "https://images.unsplash.com/photo-1622483767028-3f66f32aef97?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
